61d8c8dc4cca56392c99820ce7d44522b0299de7
Supun Liyanage <174965581+SupunLiyanage88@users.noreply.github.com>
2025-09-10 14:55:44 +0530

    Implement user authentication with JWT and add necessary DTOs and security configuration

---
+++ src/main/java/com/weatherapp/weatherapp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.weatherapp.weatherapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.weatherapp.weatherapp.entity.UserEntity;
+
+
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
+    
+}
